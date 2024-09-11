e3ee582d367b29924aa262727a6bc82c2fddd3e2
waterslinky <110861454+waterslinky@users.noreply.github.com>
2024-09-10 17:44:06 -0700

    Copy from another repo.

---
+++ credits.js
@@ -0,0 +1,24 @@
+credits_elements=[new align({ 
+    "elements":[
+
+
+        new text({"x": innerWidth/2 ,"y": 50,"text": "Quintessence:" ,"size":70,"color":"rgb(66, 242, 245)","align":"center"}),
+        new text({"x":innerWidth/2  ,"y":230 ,"text": "By: @Quintessence" ,   "size": 50  ,"color":"rgb(255,255,255)"   ,"align":  "center" }),
+        new text( {"x":innerWidth/2  ,"y":300 ,"text": "Pack B: @Calamity1956" ,   "size": 50  ,"color": "rgb(255,255,255)"  ,"align":  "center" } ),
+        new text( {"x":innerWidth/2  ,"y":370 ,"text": "Code Gremlin Played By: @duckyguy" ,   "size": 50  ,"color": "rgb(255,255,255)"  ,"align":  "center" } ),
+        new button({
+            "image":youtube_button_image,
+            "x":innerWidth/2,
+            "y":innerHeight/2,
+            "size_x":700,
+            "size_y":700*(48/63),
+            "align":"center",
+            "on_clicked":function(){
+
+            }
+
+
+        })
+    ],
+    "groups":["ui"]
+})]
