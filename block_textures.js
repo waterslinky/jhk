b04a45da7e773b5581290d9805b22d5e6935fcfd
waterslinky <110861454+waterslinky@users.noreply.github.com>
2024-09-10 17:45:35 -0700

    Copy from another repo.

---
+++ block_textures.js
@@ -0,0 +1,183 @@
+loads_started=1
+function reload_block_texters(){
+    load_as_image("blank",'blocks/blank.png')
+
+
+
+    load_as_image("magnifying_glass",'blocks/magnifying_glass.png')
+
+
+
+
+    //Mushroom Image
+    load_as_image("blue_mushroom",'blocks/mushrooms/blue_mushroom.png')
+
+    load_as_image("slidder_bar",'slidder_bar.png')
+
+
+    load_as_image("red_mushroom",'blocks/mushrooms/red_mushroom.png')
+
+    load_as_image("green_mushroom",'blocks/mushrooms/green_mushroom.png')
+
+    load_as_image("inventory",'inventory_ui.png')
+
+    
+    load_as_image("dirt",'blocks/dirt/dirt',[10,1,5,5])
+    
+    load_as_image("soil",'blocks/soil/soil',[10,1,5,5])
+
+    load_as_image("missing_block",'blocks/missing_block.png')
+
+
+    load_as_image("striped_log",'blocks/striped_log.png')
+
+
+    //Bark paths
+    load_as_image("slightly_striped_log",'blocks/slightly_striped_log/slightly_striped_log',[1,1,1])
+
+    load_as_image("moderately_striped_log",'blocks/moderately_striped_log/moderately_striped_log',[1,1,1])
+
+    load_as_image("mostly_striped_log",'blocks/mostly_striped_log/mostly_striped_log',[1,1,1])
+
+
+    
+
+    load_as_image("wheat_crop1",'blocks/wheat_crop/stages/wheat_crop1.png')
+    load_as_image("wheat_crop2",'blocks/wheat_crop/stages/wheat_crop2.png')
+    load_as_image("wheat_crop3",'blocks/wheat_crop/stages/wheat_crop3.png')
+    load_as_image("wheat_crop4",'blocks/wheat_crop/stages/wheat_crop4.png')
+
+    load_as_image("wheat_crop_bottom1",'blocks/wheat_crop/bottom/wheat_crop_bottom1.png')
+    load_as_image("wheat_crop_bottom2",'blocks/wheat_crop/bottom/wheat_crop_bottom2.png')
+    load_as_image("wheat_crop_bottom3",'blocks/wheat_crop/bottom/wheat_crop_bottom3.png')
+    load_as_image("wheat_crop_bottom4",'blocks/wheat_crop/bottom/wheat_crop_bottom4.png')
+
+    load_as_image("wheat_crop4_top",'blocks/wheat_crop/top/wheat_crop_top4.png')
+
+    load_as_image("mushroom_root",'blocks/mushrooms/mushroom_root.png')
+
+
+
+    load_as_animation("camp_fire",'blocks/camp_fire_sheet.png',16,800)
+
+
+
+
+
+
+
+    // load_as_image("bark_chunk_2_1",'blocks/bark_chunk/bark_hole_path_2/bark_hole_1.png')
+    // load_as_image("bark_chunk_2_2",'blocks/bark_chunk/bark_hole_path_2/bark_hole_2.png')
+    // load_as_image("bark_chunk_2_3",'blocks/bark_chunk/bark_hole_path_2/bark_hole_3.png')
+    // load_as_image("bark_chunk_2_4",'blocks/bark_chunk/bark_hole_path_2/bark_hole_4.png')
+
+
+    // load_as_image("bark_chunk_3_1",'blocks/bark_chunk/bark_hole_path_3/bark_hole_1.png')
+    // load_as_image("bark_chunk_3_2",'blocks/bark_chunk/bark_hole_path_3/bark_hole_2.png')
+    // load_as_image("bark_chunk_3_3",'blocks/bark_chunk/bark_hole_path_3/bark_hole_3.png')
+    // load_as_image("bark_chunk_3_4",'blocks/bark_chunk/bark_hole_path_3/bark_hole_4.png')
+
+
+
+    // bark_chunk_path_1 = [bark_chunk_1_1_image,bark_chunk_1_2_image,bark_chunk_1_3_image,bark_chunk_1_4_image]
+    // bark_chunk_path_2 = [bark_chunk_2_1_image,bark_chunk_2_2_image,bark_chunk_2_3_image,bark_chunk_2_4_image]
+    // bark_chunk_path_3 = [bark_chunk_3_1_image,bark_chunk_3_2_image,bark_chunk_3_3_image,bark_chunk_3_4_image]
+
+    // bark_chunk_paths = [bark_chunk_path_2,bark_chunk_path_3]
+
+
+
+
+
+    load_as_image("stone",'blocks/stone/stone',[1,1,1])
+
+    
+
+    load_as_image("crafting_table_left",'blocks/crafting_table/crafting_table_left.png')
+    load_as_image("crafting_table_top",'blocks/crafting_table/crafting_table_top.png')
+
+
+    load_as_image("crafting_table",'blocks/crafting_table/crafting_table.png')
+
+
+
+    load_as_image("void_block",'blocks/void_block.png')
+
+
+
+
+
+    load_as_image("break_block_sheet",'blocks/break_block_sheet.png')
+
+
+
+
+
+
+
+    load_as_image("air",'blocks/air.png')
+
+    load_as_image("filled_air",'blocks/filled_air.png')
+
+
+
+    load_as_image("grass",'blocks/grass/grass',[10,1,5,5])
+    load_as_image("snowy_grass",'blocks/snowy_grass/snowy_grass',[10,1,5,5])
+
+
+
+    load_as_image("plank",'blocks/plank.png')
+
+    
+
+
+    load_as_image("grass_block",'blocks/grass_block.png')
+
+    load_as_image("tall_grass_block",'blocks/tall_grass_block.png')
+
+    load_as_image("slate",'blocks/slate.png')
+
+    
+
+
+
+
+
+
+
+    load_as_image("stone_boulder",'blocks/stone_boulder.png')
+
+    load_as_image("marble",'blocks/marble.png')
+
+    load_as_image("stone_brick",'blocks/stone_brick.png')
+
+    load_as_image("cracked_block",'blocks/cracked_block.png') 
+
+    load_as_image("extrude_block",'blocks/extrude_block.png') 
+
+    load_as_image("load_block",'blocks/load_block.png')
+    
+    load_as_image("bedrock",'blocks/bedrock.png') 
+    load_as_image("bedrock_image2",'blocks/bedrock2.png') 
+
+    
+
+
+
+    
+
+
+    
+    
+    
+    load_as_image("log",'blocks/log.png')   
+    load_as_image("leaf",'blocks/leaf/leaf')   
+
+
+    load_as_image("leaf_side1",'blocks/leaf/leaf_side1.png')   
+
+    
+
+
+
+}
